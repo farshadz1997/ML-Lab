c77ebba08a2a8d125198af84b58266430deec03b
Farshad <Farshadzargary1997@gmail.com>
2026-03-11 14:40:52 +0330

    AppBar added to layout

--- src/ui/layout.py
+++ src/ui/layout.py
@@ -1,6 +1,6 @@
 import flet as ft
 from dataclasses import dataclass
-import warnings
+import warnings, webbrowser
 from .dataset_explorer import DatasetExplorer
 from .data_visualization import DataVisualization
 from .model_factory import ModelFactory
@@ -32,16 +32,35 @@ class AppLayout:
         self.data_science = DataVisualization(self, self.page)
         self.model_factory = ModelFactory(self, self.page)
         self.page.navigation_bar = ft.NavigationBar(
+            bgcolor=ft.Colors.SURFACE_CONTAINER_HIGHEST,
             on_change=self.on_navigation_change,
             destinations=[
-                ft.NavigationBarDestination(icon=ft.Icons.DATA_OBJECT, label="Dataset explorer"),
-                ft.NavigationBarDestination(icon=ft.Icons.ANALYTICS, label="Data visualization", disabled=True),
+                ft.NavigationBarDestination(icon=ft.Icons.DATA_OBJECT, label="Dataset Explorer"),
+                ft.NavigationBarDestination(icon=ft.Icons.ANALYTICS, label="Data Visualization", disabled=True),
                 ft.NavigationBarDestination(
                     icon=ft.Icons.MODEL_TRAINING,
                     label="Model Factory",
                     disabled=True
                 ),
             ]
+        )
+        self.toggle_theme_mode_button = ft.IconButton(
+            ft.Icons.MODE_NIGHT if self.page.theme_mode == "light" else ft.Icons.WB_SUNNY_ROUNDED,
+            on_click=self.toggle_theme_mode,
+        )
+        self.page.appbar = ft.AppBar(
+            title=ft.Text("Dataset Explorer", font_family="SF thin", text_align=ft.TextAlign.CENTER, size=25),
+            center_title=True,
+            bgcolor=ft.Colors.SURFACE_CONTAINER_HIGHEST,
+            elevation=5,
+            actions=[
+                self.toggle_theme_mode_button,
+                ft.IconButton(
+                    ft.Icons.OPEN_IN_BROWSER,
+                    tooltip="GitHub page",
+                    on_click=lambda _: webbrowser.open("https://github.com/farshadz1997/ML-Lab")
+                )
+            ],
         )
         self.page.scroll = ft.ScrollMode.ALWAYS
         self.page.controls = [self.home.build_controls()]
@@ -58,16 +77,27 @@ class AppLayout:
             duration=20000
         ))
 
+    def toggle_theme_mode(self, e: ft.ControlEvent) -> None:
+        self.page.theme_mode = "dark" if self.page.theme_mode == "light" else "light"
+        self.toggle_theme_mode_button.icon = (
+            ft.Icons.MODE_NIGHT if self.page.theme_mode == "light" else ft.Icons.WB_SUNNY_ROUNDED
+        )
+        self.page.update()
+
     def on_navigation_change(self, e: ft.ControlEvent):
         nav_index = int(e.data)
         if nav_index == 0:
             column = self.home.build_controls()
+            self.page.appbar.title.value = "Dataset Explorer"
         elif nav_index == 1:
             column = self.data_science.build_controls()
+            self.page.appbar.title.value = "Data Visualization"
         elif nav_index == 2:
             column = self.model_factory.build_controls()
+            self.page.appbar.title.value = "Model Factory"
         else:
             column = self.home.build_controls()
+            self.page.appbar.title.value = "Dataset Explorer"
         self.page.controls.clear()
         self.page.controls = [column]
         self.page.update()
